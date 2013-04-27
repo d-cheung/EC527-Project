9ceb7330c65ed5c838238a5cbe84701e890418b8
David Cheung <d.cheung.xd@gmail.com>
2013-04-26 21:02:03 -0400

    Added timing

--- Serial Code/main.cpp
+++ Serial Code/main.cpp
@@ -6,25 +6,56 @@
 #include "SurfDescriptor.h"
 #include <vector>
 #include <cmath>
+#include <time.h>
 
-using namespace std;
+#define GIG 1000000000
+#define CPG 2.53           // Cycles per GHz -- Adjust to your computer
+#define ITERATIONS 10
 
-void PaintSURF(bitmap_image &img, vector<IPoint> * ipts);
+/************************************************************************************
+*  Usage:
+*      ./surf [source file name] [destination file name]
+*
+************************************************************************************/
 
 int main(int argc, char ** argv)
 {
 	if (argc == 3)
 	{
 
-		string file_name(argv[1]);
+		void PaintSURF(bitmap_image &img, std::vector<IPoint> * ipts);
+		struct timespec diff(struct timespec start, struct timespec end);
+		int clock_gettime(clockid_t clk_id, struct timespec *tp);
+
+		struct timespec time1, time2;
+		struct timespec time_stamp[3][ITERATIONS];
+		long int i, j, k;
+		long int time_sec, time_ns;
+		const char * labels[3] = {"IntegralImage","FastHessian","SurfDescriptor"};
+
+		std::string file_name(argv[1]);
 		bitmap_image image(file_name);
-//		image.save_image("test01_saved.bmp");
 
-		printf("%d x %d\n",image.width(), image.height());
-		IntegralImage * iimg = IntegralImage::FromImage(image);
+		IntegralImage * iimg;
+		// Running IntegralImage
+		for (i = 0; i < ITERATIONS; i++)
+		{
+			clock_gettime(CLOCK_REALTIME, &time1);
+			iimg = IntegralImage::FromImage(image);
+			clock_gettime(CLOCK_REALTIME, &time2);
+			time_stamp[0][i] = diff(time1,time2);
+		}
 
-		vector<IPoint> * ipts = NULL;
-		ipts = FastHessian::getIpoints((float)0.0002, 5, 2, iimg);
+		std::vector<IPoint> * ipts = NULL;
+
+		// Running FastHessian
+		for (i = 0; i < ITERATIONS; i++)
+		{
+			clock_gettime(CLOCK_REALTIME, &time1);
+			ipts = FastHessian::getIpoints((float)0.0002, 5, 2, iimg);
+			clock_gettime(CLOCK_REALTIME, &time2);
+			time_stamp[1][i] = diff(time1,time2);
+		}
 
 		if (ipts == NULL)
 		{
@@ -32,9 +63,31 @@ int main(int argc, char ** argv)
 			return 0;
 		}
 
-		SurfDescriptor::DecribeInterestPoints(ipts, false, false, iimg);
+		// Running SurfDescriptor
+		for (i = 0; i < ITERATIONS; i++)
+		{
+			clock_gettime(CLOCK_REALTIME, &time1);
+			SurfDescriptor::DecribeInterestPoints(ipts, false, false, iimg);
+			clock_gettime(CLOCK_REALTIME, &time2);
+			time_stamp[2][i] = diff(time1,time2);
+		}
+
 		PaintSURF(image, ipts);
 		image.save_image(argv[2]);
+
+		printf("Width: %d, Height, %d\n", image.width(), image.height());
+		for (i = 0; i < 3; i++)
+		{
+			printf("%s: ", labels[i]);
+			for (j = 0; j < ITERATIONS; j++)
+			{
+				if (j != 0) printf(", ");
+
+				printf("%ld", (long int)((double)(CPG)*(double)(GIG * time_stamp[i][j].tv_sec + time_stamp[i][j].tv_nsec)));
+			}
+		printf("\n");
+		}
+
 
 		delete iimg;
 		delete ipts;
@@ -43,14 +96,30 @@ int main(int argc, char ** argv)
 
 	else
 	{
-		printf("Invalid use!  Need bitmap file\n");
+		printf("Invalid use!\n");
 	}
 
 	return 0;
 }
 
+/*************************************************/
 
-void PaintSURF(bitmap_image &img, vector<IPoint> * ipts)
+struct timespec diff(struct timespec start, struct timespec end)
+{
+  struct timespec temp;
+  if ((end.tv_nsec-start.tv_nsec)<0) {
+    temp.tv_sec = end.tv_sec-start.tv_sec-1;
+    temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+  } else {
+    temp.tv_sec = end.tv_sec-start.tv_sec;
+    temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+  }
+  return temp;
+}
+
+/*************************************************/
+
+void PaintSURF(bitmap_image &img, std::vector<IPoint> * ipts)
 {
 
 	unsigned char   red[3] = {0xFF, 0x00, 0x00};
@@ -65,7 +134,8 @@ void PaintSURF(bitmap_image &img, vector<IPoint> * ipts)
 		int R = (int)(S / (float)2.0);
 
 		int  pt[2] = {(int)(ip->x), (int)(ip->y)};
-		int ptR[2] = {(int)(R * cos(ip->orientation)), (int)(R * sin(ip->orientation))};
+		int ptR[2] = {(int)(R * cos(ip->orientation)),
+			      (int)(R * sin(ip->orientation))};
 
 		myPen[(ip->laplacian > 0 ? 2 : 0)] = 0xFF;
 		idrawer.pen_color(myPen[0], myPen[1], myPen[2]);
